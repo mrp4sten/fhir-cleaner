4efcdd2c73281cb11953cae4cde9996c448d70a0
mrp4sten <mauricio.pasten.martinez@gmail.com>
2023-10-09 10:24:11 -0600

    Getting Tasks filtered by authored-on

--- cleaner-for-old-task.sh
+++ cleaner-for-old-task.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # author mrp4sten (Mauricio Pasten)
 
+DATA_STORE_URL=$(gum input --placeholder "Enter your DataStore: ")
+AUTHORED_ON_FILTER=$(gum input --placeholder "Enter date of authored-on: ")
+BEARER_TOKEN=$(gum input --placeholder "Enter your Bearer Token: ")
+
+curl --location $DATA_STORE_URL"/?_count=1000&authored-on=$AUTHORED_ON_FILTER" \
+--header "Authorization: Bearer $BEARER_TOKEN" > task-bundle-response.json
