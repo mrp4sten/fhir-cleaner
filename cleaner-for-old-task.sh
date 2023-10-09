b58b642ef8bf177555ab46f0553d17a84b4d3da5
mrp4sten <mauricio.pasten.martinez@gmail.com>
2023-10-09 12:27:46 -0600

    Removing Task getting from Bundle

--- cleaner-for-old-task.sh
+++ cleaner-for-old-task.sh
@@ -7,3 +7,12 @@ BEARER_TOKEN=$(gum input --placeholder "Enter your Bearer Token: ")
 
 curl --location $DATA_STORE_URL"/?_count=1000&authored-on=$AUTHORED_ON_FILTER" \
 --header "Authorization: Bearer $BEARER_TOKEN" > task-bundle-response.json
+
+TASKS_ID=$(jq '[.entry[].resource.id]' task-bundle-response.json)
+
+ARRAY_TASKS_ID=($(echo "$TASKS_ID" | jq -r '.[]'))
+
+for ID in "${ARRAY_TASKS_ID[@]}"; do
+  curl --location --request DELETE $DATA_STORE_URL/$ID \
+--header "Authorization: Bearer $BEARER_TOKEN"
+done
